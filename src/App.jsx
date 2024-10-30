480afdad5bdde03ade5f78cd79e982df4028272d
YairMoller <yairmoller@hilma.tech>
2024-10-30 18:00:50 +0200

    base display component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import { useState } from "react";
 
 import "./App.css";
-
+import Display from "./components/Display";
 function App() {
-    const [charArray, setCharArray] = useState({});
+    const KeyboardTypeArray = [[], []];
+    const [KeyboardType, setKeyboardType] = useState();
+    const [charArray, setCharArray] = useState([
+        { size: 12, colors: "black", value: "a" },
+        { size: 12, colors: "black", value: "a" },
+        { size: 12, colors: "black", value: "h" },
+    ]);
     return (
         <>
-            <Display />
-            <Keyboard />
+            <Display charArray={charArray} />
+            {/* <Keyboard />
             <Languages />
             <Sizes />
             <Colors />
-            <Specials />
+            <Specials /> */}
         </>
     );
 }
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,5 +1,15 @@
 import Char from "./Char";
 
-export default function Display() {
-    return <></>;
+export default function Display({ charArray }) {
+    console.log("charArray: ", charArray);
+    console.log("charArray: ", charArray[0]);
+    console.log("charArray: ", charArray[0].size);
+
+    return (
+        <>
+            <Char size={charArray[0].size} color={charArray[0].color} value={charArray[0].value} />
+            <Char size={charArray[1].size} color={charArray[1].color} value={charArray[1].value} />
+            <Char size={charArray[2].size} color={charArray[2].color} value={charArray[2].value} />
+        </>
+    );
 }
