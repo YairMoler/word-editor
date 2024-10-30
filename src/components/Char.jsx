48232a46ceba4637d914afbb7b004266c0cac3de
YairMoller <yairmoller@hilma.tech>
2024-10-30 17:12:35 +0200

    base skeleton

---
+++ src/components/Char.jsx
@@ -0,0 +1,3 @@
+export default function Char(props) {
+    return <div style={{ size: props.size, color: props.color }}>{props.value}</div>;
+}
---
+++ src/components/Display.jsx
@@ -0,0 +1,5 @@
+import Char from "./Char";
+
+export default function Display() {
+    return <></>;
+}
