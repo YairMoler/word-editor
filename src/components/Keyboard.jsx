fde3673bcaa71532623e3a43f1c1b1406caacd66
shirel-maymon <shirelmaymon@hilma.tech>
2024-10-31 16:14:02 +0200

    style

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,12 +1,12 @@
 function Keyboard(props) {
     return (
         <div>
-            {props.KeyboardType.map((item) => (
+            {props.KeyboardType.map((item) => ( item === "\n" ? <br /> :
                 <button key={item} onClick={() => props.handleClickAdd(item)}>
                     {item}
                 </button>
             ))}
-            <button key="space" onClick={() => props.handleClickAdd(" ")}>
+            <br /><button key="space" onClick={() => props.handleClickAdd(" ")}>
                 space
             </button>
             <button key="delete" onClick={() => props.handleClickDelete()}>
