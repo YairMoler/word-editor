c7a872ba0f6a437952535f4856a1492425f3546f
YairMoller <yairmoller@hilma.tech>
2024-10-31 09:22:31 +0200

    starting keyboard data

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 
 import "./App.css";
 import Display from "./components/Display";
+import KeyboardTypeArray from "./data/keyboard-data";
 function App() {
-    const KeyboardTypeArray = [[], []];
-    const [KeyboardType, setKeyboardType] = useState();
+    const [KeyboardType, setKeyboardType] = useState(KeyboardTypeArray[0]);
     const [charArray, setCharArray] = useState([
         { size: 12, colors: "black", value: "a" },
         { size: 12, colors: "black", value: "a" },
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,15 +1,11 @@
 import Char from "./Char";
 
 export default function Display({ charArray }) {
-    console.log("charArray: ", charArray);
-    console.log("charArray: ", charArray[0]);
-    console.log("charArray: ", charArray[0].size);
-
     return (
-        <>
-            <Char size={charArray[0].size} color={charArray[0].color} value={charArray[0].value} />
-            <Char size={charArray[1].size} color={charArray[1].color} value={charArray[1].value} />
-            <Char size={charArray[2].size} color={charArray[2].color} value={charArray[2].value} />
-        </>
+        <div>
+            {charArray.map((char, index) => (
+                <Char size={char.size} color={char.color} value={char.value} key={index} />
+            ))}
+        </div>
     );
 }
